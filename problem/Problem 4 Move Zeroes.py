23ce51fcb741e47eb4e684120d8668f8c8ba0ef2
GrvExplorer <icodelife307@gmail.com>
2024-11-22 20:16:40 +0530

    solved -> move problems to end

--- problem/Problem 4 Move Zeroes.py
+++ problem/Problem 4 Move Zeroes.py
@@ -1,7 +1,21 @@
+
+
 class Solution(object):
     def moveZeroes(self, nums: list):
-      nums.sort()
+      for x in range(0, len(nums)):
+       try:
+        nums.index(0) 
+        if True:
+          nums.remove(0)
+          nums.append(0)
+          continue
+       except:
+         break
+      return
 
+zeroes = Solution()
 
-zeroes = Solution([0,1,0,3,12])
+ran_zeros = [0]
+zeroes.moveZeroes(ran_zeros)
+print(ran_zeros)
         
